d83296c01e183ad950fb77e6f1e6ebcb36008fed
titanism <101466223+titanism@users.noreply.github.com>
2023-03-30 23:21:39 -0500

    fix: added tableAjaxFormReloaded as global window event

--- src/ajax-form.js
+++ src/ajax-form.js
@@ -352,7 +352,7 @@ const ajaxForm = async (ev) => {
       $table.html(response.body.table);
 
       // dispatch an event so we can re-render things (e.g. dayjs)
-      $form.dispatchEvent(new Event('reload'));
+      window.dispatchEvent(new Event('tableAjaxFormReloaded'));
 
       spinner.hide();
     } else {
