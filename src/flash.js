4199246c4b94972543686e8cfb52307da396cbac
titanism <101466223+titanism@users.noreply.github.com>
2023-11-26 13:48:50 -0600

    fix: fullscreen should render immediately

--- src/flash.js
+++ src/flash.js
@@ -9,7 +9,13 @@ module.exports = () => {
       if (messages.length === 0) continue;
       for (const message of messages) {
         if (type === 'custom' && typeof message === 'object') {
-          steps.push(message);
+          // render any full screen alerts immediately
+          // eslint-disable-next-line max-depth
+          if (message.grow === 'fullscreen') {
+            Swal.fire(message);
+          } else {
+            steps.push(message);
+          }
         } else if (typeof message === 'string') {
           steps.push({
             title:
